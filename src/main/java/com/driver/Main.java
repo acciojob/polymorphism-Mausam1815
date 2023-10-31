e286c8dee591e5e3f772c64e9eaab7e7d9049ea2
Mausam Raj <128462297+Mausam1815@users.noreply.github.com>
2023-10-31 20:25:40 +0530

    assignment task completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(5, 10));
+        System.out.println(p.product(5, 10, 20));
+        System.out.println(p.doubleProduct(5.5, 10.5));
+    }
+}
+class Product {
+    public int product(int x, int y) {
+        return x * y;
+    }
 
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    public double doubleProduct(double x, double y) {
+        return x * y;
+    }
 }
